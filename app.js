cea89c61da118b94812fc58a8b27703108f21a84
Zuxiang Wang <zuxiangwang2002@gmail.com>
2023-01-04 15:33:47 -0500

    menu plain

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+// nav-bar
 const links = document.querySelector(".nav-links");
 const navBtnDOM = document.querySelector(".nav-btn");
 
 navBtnDOM.addEventListener("click", () => {
   links.classList.toggle("show-links");
-  console.log("Hello World");
 });
+
+// faq
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const currElem = e.currentTarget;
+    const children = currElem.children;
+    children[0].classList.toggle("hide");
+    children[1].classList.toggle("show-btn");
+    currElem.parentElement.nextElementSibling.classList.toggle(
+      "question-show-answer"
+    );
+  });
+});
+
+//menu
+
+const menuItems = [{}, {}];
